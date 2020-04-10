cc2acc3c5cd85bc73028d4d5c66e1f950830741e
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2020-04-10 23:49:11 +0200

    initial commit

---
+++ humanophone-master.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+import sys, pygame, pygame.midi
+
+import smbus
+import errno
+
+from pychord import *
+
+from audiolazy import *
+
+class Note:
+    def __init__(self, name, velocity):
+        self.name = name
+        self.velocity = velocity
+
+    def __repr__(self):
+        return str(self.name) + " " + str(self.velocity)
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, other):
+        return other.name == self.name
+
+if __name__ == "__main__":
+    bus_number = 1
+    bus = smbus.SMBus(bus_number)
+    device_count = 0
+
+    addresses = num_devices(bus)
+    if len(addresses) == 0:
+        return
+
+    # set up pygame
+    pygame.init()
+    pygame.midi.init()
+
+    # list all midi devices
+    for x in range(0, pygame.midi.get_count()):
+        print(pygame.midi.get_device_info(x))
+
+    # open a specific midi device
+    inp = pygame.midi.Input(3)
+
+    input_notes = set()
+    playing_notes = set()
+
+    while True:
+        if inp.poll():
+            events = inp.read(1000)
+            for event in events:
+                note_name = midi2str(event[0][1], False)
+                on_event = False;
+                if event[0][0] == 144:
+                    on_event = True
+                elif event[0][0] == 128:
+                    on_event = False
+                else:
+                    continue
+                velocity = event[0][2]
+                note = Note(note_name, velocity)
+                if on_event:
+                    input_notes.add(note)
+                    freq = str2freq(note.name)
+                    print(freq)
+                    bus.write_word_data(0x22, 0x55, int(freq))
+                else:
+                    input_notes.discard(note)
+                print(input_notes)
+
+        pygame.time.wait(10)
+
+def num_devices(bus):
+    addresses = []
+    for device in range(3, 128):
+        try:
+            bus.write_byte(device, 0)
+            addresses.append(device)
+        except IOError as e:
+            if e.errno != errno.EREMOTEIO:
+                print("Error: {0} on address {1}".format(e, hex(device)))
+        except Exception as e: # exception if read_byte fails
+            print("Error unk: {0} on address {1}".format(e, hex(device)))
+    return addresses
+
